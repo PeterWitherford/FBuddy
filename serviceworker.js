33cc4db57811abc9d63c07150fd11b540a105e2c
Peter Witherford <Peter Witherford>
2017-03-21 11:49:28 +0000

    serviceworker

--- serviceworker.js
+++ serviceworker.js
@@ -1,7 +1,7 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('gih-cache').then(function(cache) {
-      return cache.add('offline.html');
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match('offline.html');
     })
   );
 });
